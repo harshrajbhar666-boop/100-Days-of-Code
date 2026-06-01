09aa2892c0f9ed3cb1dfbc70abdb4fc2cd212d74
Harsh Rajbhar <harshrajbhar666@gmail.com>
2026-06-01 11:44:56 +0530

    Add files via upload

---
+++ Day_3.py
@@ -0,0 +1,42 @@
+# write a  python program to add to number
+
+
+a = 23
+b = 45
+
+c = a + b
+print(c) 
+
+# write a python program to find remander when a number is divided by z
+
+a = 50
+b = 3
+
+c=a%b
+
+print(c)
+
+#check the type of varibal assigned using input ( ) function
+
+my_teem=input()
+
+print(type(my_teem))
+
+# write a python program to find an average 
+
+a=50
+b=35
+
+c=(a+b)/2
+
+print(c)
+
+# write a python program to calculate the square of a number 
+
+number=4
+
+square=number*number
+
+print(squqre)
+
+
