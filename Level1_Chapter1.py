c83bee2180ad60a7b70bfa34f0e5c55a4c9e52b0
Harsh Rajbhar <harshrajbhar666@gmail.com>
2026-06-30 12:12:14 +0530

    "Level 1 - Chapter 1: Mastered Variables, Memory Identity, Type Casting, and Operators"

---
+++ Level1_Chapter1.py
@@ -0,0 +1,49 @@
+# The Memory Secret
+a = 6
+b = 6
+
+print(id(a))
+print(id(b))
+
+print(a is b)
+
+
+# Data Type Pata Karna
+
+
+naam = "Harsh"
+marks = 85
+package = 50.0  
+status = True
+
+
+print(type(naam))
+print(type(marks))
+print(type(package))
+print(type(status))
+
+# Type Casting 
+
+
+my_package = 50.7
+converted_package = int(my_package)
+print("Converted Value:", converted_package)
+print("New Type:", type(converted_package))
+
+
+string_number = "100"
+real_number = int(string_number)
+print("String to Int:", real_number + 50)
+
+# Operators
+
+
+x = 7
+y = 3
+
+print("Normal Division (/):", x / y)
+print("Floor Division (//):", x // y)
+print("Modulus (%):", x % y)
+
+
+print("Kya x, y se bada hai?:", x > y)
